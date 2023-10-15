b1733f41d6c3c6df882cbeb7260d0f0d4217d420
Lucky Tambe <lucky18tambe@gmail.com>
2023-10-15 22:48:34 +0530

    C#

---
+++ C#Programs/GeometricalShapeExample.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometricalShapeExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int choice, num, square, l, b, rectangle;
+            float r, circle;
+            Console.WriteLine("1.Circle\n2.Square\n3.Rectangle");
+            choice = Convert.ToInt32(Console.ReadLine());
+
+            if (choice == 1)
+            {
+                Console.WriteLine("Enter Radius");
+                r = Convert.ToSingle(Console.ReadLine());
+
+                circle = 3.14f * r * r;
+                Console.WriteLine("Circle is " + circle);
+
+
+            }
+            else if (choice == 2)
+            {
+                Console.WriteLine("Enter Num");
+                num = Convert.ToInt32(Console.ReadLine());
+
+                square = num * num;
+                Console.WriteLine("Square is " + square);
+
+            }
+            else if (choice == 3)
+            {
+                Console.WriteLine("Enter Length");
+                l = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Enter Breadth");
+                b = Convert.ToInt32(Console.ReadLine());
+
+                rectangle = l * b;
+                Console.WriteLine("Rectangle is " + rectangle);
+
+            }
+            else
+            {
+                Console.WriteLine("Invalid days");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ C#Programs/OperatorsIfElseExample.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsIfElseExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, Choice, total;
+
+            Console.WriteLine("Enter num1");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("ENter num2");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("1.Addition\n2.Substract\n3.Multiply\n4.Divide\n5.Exit");
+            Choice = Convert.ToInt32(Console.ReadLine());
+
+
+            if (Choice == 1)
+            {
+                total = num1 + num2;
+                Console.WriteLine(" Addition = " + total);
+            }
+            else if(Choice == 2)
+            {
+                total = num1 - num2;
+                Console.WriteLine(" Subtration = " + total);
+            }
+            else if (Choice == 3)
+            {
+                total = num1* num2;
+                Console.WriteLine(" Subtration = " + total);
+            }
+            else if (Choice == 4)
+            {
+                total = num1 / num2;
+                Console.WriteLine(" Subtration = " + total);
+            }
+            else
+            {
+                Console.WriteLine("Invalid Number");
+            }
+
+
+
+
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
