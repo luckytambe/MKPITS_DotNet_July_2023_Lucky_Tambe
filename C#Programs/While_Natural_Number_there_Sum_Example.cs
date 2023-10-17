bfc5fa256583ecb7f52d26ef718f55f154ef301c
Lucky Tambe <lucky18tambe@gmail.com>
2023-10-17 21:52:46 +0530

    C#

---
+++ C#Programs/While_Natural_Number_there_Sum_Example.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace While_Natural_Number_there_Sum_Example
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, counter = 1, sum = 0;
+            Console.WriteLine("Enter num: ");
+            num = Convert.ToInt32(Console.ReadLine());
+            while (counter <= num)
+            {
+                if (counter % 2 != 0)
+                {
+                    Console.Write("{0} ", counter);
+                    sum = sum + counter;
+                }
+                counter++;
+            }
+            Console.WriteLine("\nSum is " + sum);
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ C#Programs/While_Prime_Number_Example.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace While_Prime_Number_Example
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 2, num1, num = 0;
+            Console.WriteLine("Prime numbers between 1 and 20 : ");
+            while (counter < 20)
+            {
+                int isprime = 1;
+
+                num1 = 2;
+                while (num1 <= counter / 2)
+                {
+                    if (counter % num1 == 0)
+                    {
+                        isprime = 0;
+                        break;
+                    }
+                    num1++;
+                }
+                if (isprime == 1)
+                {
+                    Console.WriteLine(counter);
+                }
+                counter++;
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
