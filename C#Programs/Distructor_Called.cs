f700181e01a1cf9f50f91bcbbdde005eb32fe640
Lucky Tambe <lucky18tambe@gmail.com>
2023-11-07 17:17:23 +0530

    C#

---
+++ C#Programs/Distructor_Called.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Distructor_Called
+{
+    class Employe
+    {
+        public Employe()
+        
+        {
+            Console.WriteLine("Constructor Called");
+        }
+        ~Employe()
+        {
+            Console.WriteLine("Distructor Called");
+        }
+    }
+    
+    internal class Program 
+    {
+        static void Main(string[] args)
+        {
+            Employe emp=new Employe();
+            Console.ReadKey(); 
+            
+            Employe emp2=new Employe(); 
+            Console.ReadKey();
+            
+        }
+    }
+}
---
+++ C#Programs/Method_Overloading_With_Different_Types_Parameter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Overloading_With_Different_Types_Parameter
+{
+    class Calculate
+    {
+      public void addition(int n1, int n2) 
+        {
+            int res = 0;
+            res = n1 + n2;
+            Console.WriteLine("Res = " + res);
+        }
+        public void addition(float n1, float n2)
+        {
+            float res = 0;
+            res = n1 + n2;
+            Console.WriteLine("Res = " + res);
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculate calc = new Calculate();
+
+            calc.addition(2, 4);
+
+            Console.WriteLine("For Float");
+
+            calc.addition(8.5f, 4.8f);
+
+            Console.ReadKey();
+
+        }
+    }
+}
