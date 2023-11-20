1f3870c1669e50882f196413afc2039b3ebccb32
Lucky Tambe <lucky18tambe@gmail.com>
2023-11-20 23:01:47 +0530

    C#

---
+++ C#Programs/Assign_Abstract1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assign_Abstract
+{
+    abstract class shape
+    {
+        public abstract void draw();
+       
+    }
+
+    class circle : shape
+    {
+        public override void draw()
+        {
+            Console.WriteLine("-------Circle method-------");
+        }
+    }
+
+   
+    class square : shape
+    {
+        public override void draw()
+        {
+            Console.WriteLine("------Square method------");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            shape c = new circle();
+            c.draw();
+
+            shape s = new square(); 
+            s.draw();
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
