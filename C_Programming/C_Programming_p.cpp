d2cfc124f3b3025b4ab4ff4dc5372484c7019ce1
Lucky Tambe <lucky18tambe@gmail.com>
2023-12-05 17:12:39 +0530

    C

---
+++ C_Programming/C_Programming_p.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+
+{
+	int num , square ; 
+	printf("Enter number ");
+	scanf("%d",&num);
+	
+	square = num * num ;
+	
+	printf("Square = " + square);
+	
+}
---
+++ C_Programming/Square_pro.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+	int num;
+	
+	printf("enter number");
+	scanf("%d " , &num);
+	
+	int square = num * num ;
+
+	
+	printf("square = ", square);
+}
